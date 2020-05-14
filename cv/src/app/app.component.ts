efc807249cbf6b60452b325d71ebab96a41acbdc
Randika Rodrigo <57132743+RandikaR21@users.noreply.github.com>
2020-05-15 00:41:30 +0530

    First Day, Basic structure

--- cv/src/app/app.component.ts
+++ cv/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'cv';
+  loadComponent : number = 0;
+  public color;
+
+  selectedComponent(i : number){
+    this.loadComponent=i;
+  }
+
 }
--- cv/src/app/app.module.ts
+++ cv/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
