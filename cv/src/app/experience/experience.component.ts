a0447849d3d4c9db04d0fd008c1942672b75b8bf
Randika Rodrigo <57132743+RandikaR21@users.noreply.github.com>
2020-05-24 22:42:53 +0530

    Added competitions

--- cv/src/app/experience/experience.component.ts
+++ cv/src/app/experience/experience.component.ts
@@ -12,4 +12,28 @@ export class ExperienceComponent implements OnInit {
   ngOnInit() {
   }
 
+  competitions=[
+    {
+      "name" : "IEEE Xtreme V13.0",
+      "rank" : "Gobal rank - 603, Country rank - 39"
+    },
+    {
+      "name" : "Google Hashcode 2020",
+      "rank" : "participant"
+    },
+    {
+      "name" : "Aces Coders V8.0",
+      "rank" : "participant"
+    },
+    {
+      "name" : "SLIIT Codefest",
+      "rank" : "semi-finalist"
+    },
+    {
+      "name" : "IEEE Coderally",
+      "rank" : "participant"
+    }
+
+  ]
+
 }
